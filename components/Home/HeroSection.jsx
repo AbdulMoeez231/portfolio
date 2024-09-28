046183560171cf8ff30906c0385f0ba2e4b8c9f3
AbdulMoeez231 <moeezbuttab231@gmail.com>
2024-09-29 04:36:17 +0500

    animation added

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 
 const HeroSection = () => {
     return (
-        <div className='container fade-in'>
-            <h1 className='text-white text-3xl sm:text-4xl font-extrabold mt-20' style={{ lineHeight: "55px" }}>Hi, <br className='sm:hidden' /> I'm <span className='text-gradient'>Abdul Moeez.</span> <br />
-                Full stack developer based in <br /> Lahore, Pakistan.</h1>
-            <p className='text-white text-opacity-80 max-w-[600px] leading-relaxed mt-5'>As a developer, I enjoy making concepts come to life on the internet. I'm all about making enjoyable user experiences, whether it be by designing slick user interfaces or developing reliable back-end systems.</p>
-            <p className='mt-5 text-white'>Get in touch <span className='relative'><div className='absolute w-full h-[2px] bg-gradient left-0 -bottom-1'></div> <a href="mailto:moeezbuttab231@gmail.com">hello@abdulmoeez.me</a></span></p>
+        <div className='container flex items-end justify-between fade-in'>
+            <div>
+
+                <h1 className='text-white text-3xl sm:text-4xl font-extrabold mt-20' style={{ lineHeight: "55px" }}>Hi, <br className='sm:hidden' /> I'm <span className='text-gradient'>Abdul Moeez.</span> <br />
+                    Full stack developer based in <br /> Lahore, Pakistan.</h1>
+                <p className='text-white text-opacity-80 max-w-[600px] leading-relaxed mt-5'>As a developer, I enjoy making concepts come to life on the internet. I'm all about making enjoyable user experiences, whether it be by designing slick user interfaces or developing reliable back-end systems.</p>
+                <p className='mt-5 text-white'>Get in touch <span className='relative'><div className='absolute w-full h-[2px] bg-gradient left-0 -bottom-1'></div> <a href="mailto:moeezbuttab231@gmail.com">hello@abdulmoeez.me</a></span></p>
+            </div>
+            <div className='max-lg:hidden'>
+                <div className='icon-animation'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="350" height="350" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-code">
+                        <defs>
+                            <linearGradient id="gradient1" x1="0%" x2="100%" y1="0%" y2="0%">
+                                <stop offset="0%" stop-color="rgb(255, 7, 7)" />
+                                <stop offset="100%" stop-color="rgb(255, 255, 5)" />
+                            </linearGradient>
+                        </defs>
+                        <path stroke="url(#gradient1)" d="M7 8l-4 4l4 4" />
+                        <path stroke="yellow" d="M14 4l-4 16" />
+                        <path stroke="url(#gradient1)" d="M17 8l4 4l-4 4" />
+                    </svg>
+                </div>
+            </div>
         </div>
     )
 }
