aedb9ab90aafc8f8a4b06ec0fa63fdec2e8501e8
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-02-12 01:00:46 +0500

    Add custom scrollbar styles and update CV link to Upwork profile in Navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
               style={highlightStyle}
             />
           </ul>
-          <a href="/moeez-cv.pdf" download className="gradient px-2 py-0.5 rounded-sm font-medium text-[10px] xs:text-sm md:text-base whitespace-nowrap active:scale-75 duration-200">Download CV</a>
+          <a href="https://www.upwork.com/freelancers/~010b08963f1fe8548f" target="_blank" className="gradient px-3 py-0.5 rounded-sm font-medium text-[10px] xs:text-sm md:text-base whitespace-nowrap active:scale-75 duration-200">Hire Me</a>
         </div>
       </div>
     </>
